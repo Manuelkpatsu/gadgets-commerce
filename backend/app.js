7843c8d9a18574fe81cc57d5af8f6c6a2c375af1
ManuelKpatsu <manuelkpatsu@gmail.com>
2021-11-02 08:29:21 +0000

    Add error middleware

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const morgan = require('morgan')
 const connectDB = require('./config/db')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 
 connectDB()
 
 const app = express()
 
 app.use(express.json())
+
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
+
+app.use(notFound)
+app.use(errorHandler)
 
 module.exports = app
