21b32ed7486ffc23f366e0ce393c97608ebc6cfb
ManuelKpatsu <manuelkpatsu@gmail.com>
2021-11-03 06:19:07 +0000

    Update Home, Product and Product List screens

--- client/src/components/ProductCarousel.js
+++ client/src/components/ProductCarousel.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Carousel, Image } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
